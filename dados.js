f7992dc1a9964e60db021aaae74333e669eca032
Sthéfane Karla <93161577+sthefanekarla@users.noreply.github.com>
2024-09-09 21:50:09 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+    titulo : "Rayssa Leal",
+    descricao : "Rayssa Leal, conhecida como Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento precoce. Aos 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil, conquistando o bronze nos Jogos Olímpicos de Tóquio 2020. Sua habilidade e carisma a tornaram um ícone do skate mundial.",
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags: "skate fada sk8"
+},
+{
+    titulo: "Beatriz Souza",
+    descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro na categoria +78kg. Sua técnica impecável e determinação a colocaram no topo do pódio, inspirando uma nova geração de judocas brasileiras.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    tags: "judô judo judoca judoka"
+},
+{
+    titulo: "Rebeca Andrade",
+    descricao: "Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros com suas performances impecáveis. Campeã olímpica e recordista de medalhas para o Brasil em Jogos Olímpicos, Rebeca se tornou um símbolo de superação e determinação. Sua trajetória inspiradora, marcada por talento, disciplina e um sorriso contagiante, a transformou em um dos maiores nomes do esporte brasileiro. Com suas acrobacias incríveis e a força de vontade inabalável, Rebeca continua a fazer história na ginástica mundial.",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags: "ginástica ginastica ginasta ouro"
+}
+];
